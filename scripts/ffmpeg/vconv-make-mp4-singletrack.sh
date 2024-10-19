b6a537373fe9aa0ef32d45e0126aea3d0ac47c6c
Alain <alainphm@gmail.com>
2024-10-19 18:16:37 +0200

    fix audiostream mapping

--- scripts/ffmpeg/vconv-make-mp4-singletrack.sh
+++ scripts/ffmpeg/vconv-make-mp4-singletrack.sh
@@ -2,8 +2,8 @@
 extension="${1##*.}"
 filename="${1%.*}"
 videostream=${2:-"0:v:0"}
-audiostream=${2:-"0:a:1"}
-format=${3:-mp4}
+audiostream=${3:-"0:a:0"}
+format=${4:-mp4}
 
 echo "ffmpeg -i $1 -map ${videostream} -map ${audiostream} -vcodec copy -acodec copy -movflags +faststart ${filename}.${format}"
 
