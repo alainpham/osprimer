50497e4abddac89e90afc9ca3f89cfcbb7d925ab
Alain Pham <apham@work>
2024-12-15 11:43:42 +0100

    clean sound

--- scripts/pulseaudio/install.sh
+++ scripts/pulseaudio/install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-sudo cp confsnd /usr/local/bin/confsnd
-sudo cp confpw /usr/local/bin/confpw
-sudo cp confpa /usr/local/bin/confpa
-sudo cp confsnddefault /usr/local/bin/confsnddefault
-sudo cp sndsel /usr/local/bin/sndsel
-sudo cp clrmix /usr/local/bin/clrmix
-sudo cp clrmixoff /usr/local/bin/clrmixoff
+files="snd asnd asnddef csndfoczv csndzv csndh6 csndint clrmix clrmixoff"
+for file in $files; do
+    sudo cp $file /usr/local/bin/$file
+done
+
 sudo cp pulsepod.pa /etc/pulse/default.pa.d/pulsepod.pa 
 sudo cp 89-pulseaudio-udev.rules /etc/udev/rules.d/89-pulseaudio-udev.rules
 
 # pipewire
-cp pipewire-pulse.conf ~/.config/pipewire/pipewire-pulse.conf
-cp pipewire.conf ~/.config/pipewire/pipewire.conf
-cp podcast.conf ~/.config/pipewire/pipewire.conf.d/podcast.conf
-cp 50-alsa-config.lua ~/.config/wireplumber/main.lua.d/50-alsa-config.lua
+# cp pipewire-pulse.conf ~/.config/pipewire/pipewire-pulse.conf
+# cp pipewire.conf ~/.config/pipewire/pipewire.conf
+# cp podcast.conf ~/.config/pipewire/pipewire.conf.d/podcast.conf
+# cp 50-alsa-config.lua ~/.config/wireplumber/main.lua.d/50-alsa-config.lua
+
+
+
+# backup 
+# sudo cp confpw /usr/local/bin/confpw
+# sudo cp confpa /usr/local/bin/confpa
