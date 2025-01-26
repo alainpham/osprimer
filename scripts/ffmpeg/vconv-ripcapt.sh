7980abac0beeaeeed3332ec434cd703d631d4f9f
Alain Pham <apham@work>
2025-01-26 02:41:34 +0100

    vconv capture script

--- scripts/ffmpeg/vconv-ripcapt.sh
+++ scripts/ffmpeg/vconv-ripcapt.sh
@@ -7,4 +7,4 @@ ab=${2:-160}
 
 capdev=$(v4l2-ctl --list-devices | grep -A 1 "HD60" | grep "/dev/video" | awk '{print $1}')
 
-ffmpeg -f v4l2 -fpsprobesize 120 -i $capdev -f pulse -ac 2  -i alsa_input.usb-Elgato_Game_Capture_HD60_S__0007798D4B000-03.analog-stereo -preset fast -pix_fmt yuv420p  -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k -f tee "$filename|[f=nut]-" | ffplay -
+ffmpeg -f v4l2 -thread_queue_size 1024 -fpsprobesize 120 -i $capdev -f pulse -thread_queue_size 1024 -ac 2 -i alsa_input.usb-Elgato_Game_Capture_HD60_S__0007798D4B000-03.analog-stereo  -preset fast -pix_fmt yuv420p -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k $filename
