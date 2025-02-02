983f3ce245d65a2483629aeef2c1d2272cac503c
apham <alainphm@gmail.com>
2025-02-02 16:29:19 +0100

    duration

---
+++ readme-windows.md
@@ -0,0 +1,40 @@
+setup task bar remove all shinnanigans
+install chrome set as default. login to bitwarden
+run windows update
+set folders to show extensions
+check device manager and insall missing drivers after update has completed
+rename computer
+
+install https://openhardwaremonitor.org/
+uninstall useless stuff
+
+OBS from website
+
+install winget from store on windows 10
+https://github.com/microsoft/winget-cli/releases
+
+winget install ffmpeg
+winget install -e --id KDE.Kdenlive
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Microsoft.OpenJDK.17
+winget install -e --id Zoom.Zoom
+winget install -e --id 7zip.7zip
+winget install -e --id GIMP.GIMP
+winget install -e --id VideoLAN.VLC
+vs code from official website 
+kate from store
+
+Terminal app
+
+wsl --install
+
+https://www.nirsoft.net/utils/multimonitortool-x64.zip
+multi mon tool
+
+
+installing scripts to rip record
+
+
+wget -O /mnt/c/recordings/wconv-ripcapt.bat https://raw.githubusercontent.com/alainpham/de
+bian-os-image/refs/heads/master/scripts/ffmpeg/wconv-ripcapt.bat
+
--- scripts/ffmpeg/wconv-ripcapt.bat
+++ scripts/ffmpeg/wconv-ripcapt.bat
@@ -1,24 +1,21 @@
 @echo off
 setlocal
 
-SET HOUR=%time:~0,2%
-SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
-SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+set filename=%1
+if "%filename%"=="" set filename="out.mkv"
 
-if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
+:: Set default values for the arguments (preset, crf, ab) by default run for 3h30 max
 
-ECHO %dtStamp%
+set duration=%2
+if "%duration%"=="" set duration="03:45:00"
 
-set filename=%dtStamp%.mkv
-
-:: Set default values for the arguments (preset, crf, ab)
-set preset=%1
+set preset=%3
 if "%preset%"=="" set preset=faster
 
-set crf=%2
+set crf=%4
 if "%crf%"=="" set crf=23
 
-set ab=%3
+set ab=%5
 if "%ab%"=="" set ab=160
 
 :: show devices : ffmpeg -f dshow -list_devices true -i dummy
@@ -27,12 +24,13 @@ if "%ab%"=="" set ab=160
 
 :: Run ffmpeg to capture video and audio
 
+:: -max_interleave_delta 0 if cluster error
 
-ffmpeg ^
-    -f dshow -rtbufsize 64M ^
+ffmpeg -y ^
+    -t %duration% ^
+    -f dshow -rtbufsize 512M ^
         -pixel_format yuv420p ^
         -channels 2 -sample_rate 48000 -sample_size 16 ^
         -i video="Game Capture HD60 S+":audio="Digital Audio Interface (Game Capture HD60 S+)" ^
     -f matroska -preset %preset% -pix_fmt yuv420p -c:v libx264 -crf %crf% -g 24 -c:a aac -b:a %ab%k %filename%
-
 endlocal
