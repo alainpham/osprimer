58f487f8c4c752797dbddffdd9384f20a6b4723a
apham <alainphm@gmail.com>
2025-02-17 12:16:22 +0100

    cfr ffmpeg

--- scripts/ffmpeg/wconv-ripcapt.bat
+++ scripts/ffmpeg/wconv-ripcapt.bat
@@ -9,13 +9,16 @@ if "%filename%"=="" set filename="out.mkv"
 set duration=%2
 if "%duration%"=="" set duration="03:35:00"
 
-set preset=%3
+set framerate=%3
+if "%framerate%"=="" set framerate="24000/1001"
+
+set preset=%4
 if "%preset%"=="" set preset=faster
 
-set crf=%4
+set crf=%5
 if "%crf%"=="" set crf=23
 
-set ab=%5
+set ab=%6
 if "%ab%"=="" set ab=160
 
 :: show devices : ffmpeg -f dshow -list_devices true -i dummy
@@ -32,5 +35,5 @@ ffmpeg -y ^
         -pixel_format yuv420p ^
         -channels 2 -sample_rate 48000 -sample_size 16 ^
         -i video="Game Capture HD60 S+":audio="Digital Audio Interface (Game Capture HD60 S+)" ^
-    -f matroska -max_interleave_delta 0 -preset %preset% -pix_fmt yuv420p -c:v libx264 -crf %crf% -g 24 -c:a aac -b:a %ab%k %filename%
+    -f matroska -r %framerate% -fps_mode cfr -max_interleave_delta 0 -preset %preset% -pix_fmt yuv420p -c:v libx264 -crf %crf% -g 24 -c:a aac -b:a %ab%k %filename%
 endlocal
