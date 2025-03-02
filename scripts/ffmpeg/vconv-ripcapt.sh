976d596b6b6fa4c1b465dd565ecffd8bd76240d0
Alain Pham <apham@work>
2025-03-02 16:23:49 +0100

    aac 44.1khz

--- scripts/ffmpeg/vconv-ripcapt.sh
+++ scripts/ffmpeg/vconv-ripcapt.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 filename=$(date +"%Y-%m-%d_%H-%M-%S").mkv
 duration=${1:-"03:35:00"}
-pixel_format=${2:-"yuv420p"}
-framerate=${3:-"24000/1001"}
-preset=${4:-faster}
-crf=${5:-23}
-ab=${6:-160}
+snddriver=${2:-"alsa"}
+pixel_format=${3:-"yuv420p"}
+framerate=${4:-"24000/1001"}
+preset=${5:-faster}
+crf=${6:-23}
+ab=${7:-160}
 
 capdev=$(v4l2-ctl --list-devices | grep -A 1 "HD60" | grep "/dev/video" | awk '{print $1}')
-alsadev="hw:CARD=S,DEV=0"
-pulsdev="alsa_input.usb-Elgato_Game_Capture_HD60_S__0007798D4B000-03.analog-stereo"
+
+if [ "$snddriver" == "alsa" ]; then
+    snddev="hw:CARD=S,DEV=0"
+    sndopt=""
+elif [ "$snddriver" == "pulse" ]; then
+    snddev="alsa_input.usb-Elgato_Game_Capture_HD60_S__0007798D4B000-03.analog-stereo"
+    sndopt="-thread_queue_size 1024 -use_wallclock_as_timestamps 1"
+else
+    echo "Invalid sound driver"
+    exit 1
+fi
 # ffmpeg -f v4l2 -thread_queue_size 1024 -i $capdev -f pulse -thread_queue_size 1024 -ac 2 -i alsa_input.usb-Elgato_Game_Capture_HD60_S__0007798D4B000-03.analog-stereo -f matroska  -preset fast -pix_fmt yuv420p -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k $filename
 
 
@@ -29,20 +39,23 @@ pulsdev="alsa_input.usb-Elgato_Game_Capture_HD60_S__0007798D4B000-03.analog-ster
 #     -f matroska -map 0:a -map 1:v -max_muxing_queue_size 1024 -preset $preset -pix_fmt yuv420p -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k $filename
 
 echo "
-ffmpeg  
-    -f alsa 
-        -i $alsadev 
-    -f v4l2
-        -use_wallclock_as_timestamps 1 
-        -i $capdev 
-    -f matroska -t ${duration} -r ${framerate} -fps_mode cfr -map 0:a -map 1:v -max_muxing_queue_size 1024 -preset $preset -pix_fmt yuv420p -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k $filename
-">$filename.command
-
 ffmpeg  \
-    -f alsa \
-        -i $alsadev \
+    -f ${snddriver} \
+        ${sndopt} \
+        -i ${snddev} \
     -f v4l2 \
         -thread_queue_size 1024 \
         -use_wallclock_as_timestamps 1 \
         -i $capdev \
-    -f matroska -t ${duration} -r ${framerate} -fps_mode cfr -map 0:a -map 1:v -max_muxing_queue_size 1024 -preset $preset -pix_fmt yuv420p -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k $filename
+    -f matroska -t ${duration} -r ${framerate} -fps_mode cfr -map 0:a -map 1:v -preset $preset -pix_fmt yuv420p -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k  $filename
+">$filename.command
+
+ffmpeg  \
+    -f ${snddriver} \
+        ${sndopt} \
+        -i ${snddev} \
+    -f v4l2 \
+        -thread_queue_size 1024 \
+        -use_wallclock_as_timestamps 1 \
+        -i $capdev \
+    -f matroska -t ${duration} -r ${framerate} -fps_mode cfr -map 0:a -map 1:v -preset $preset -pix_fmt yuv420p -c:v libx264 -crf ${crf} -g 24 -c:a aac -b:a ${ab}k -ar 44100 $filename
