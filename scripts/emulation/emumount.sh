722a02b273956f252b9492b7a5f3052a37ff01b7
Alain Pham <apham@work>
2025-05-02 15:49:08 +0200

    emumount

--- scripts/emulation/emumount.sh
+++ scripts/emulation/emumount.sh
@@ -1,2 +1,4 @@
 sshfs awon:/mnt/extdrv03/apps/batocera/userdata/roms /home/${TARGET_USERNAME}/ROMs
 sshfs awon:/mnt/extdrv03/apps/retroarch/downloaded_media /home/${TARGET_USERNAME}/ES-DE/downloaded_media
+sshfs awon:/mnt/extdrv03/apps/retroarch/states /home/${TARGET_USERNAME}/.config/retroarch/states
+sshfs awon:/mnt/extdrv03/apps/retroarch/saves /home/${TARGET_USERNAME}/.config/retroarch/saves
--- scripts/emulation/emustop.sh
+++ scripts/emulation/emustop.sh
@@ -1,3 +1,4 @@
 fusermount3 -u /home/${TARGET_USERNAME}/ROMs
-fusermount3 -u /home/${TARGET_USERNAME}/.config/retroarch/system
 fusermount3 -u /home/${TARGET_USERNAME}/ES-DE/downloaded_media
+fusermount3 -u /home/${TARGET_USERNAME}/.config/retroarch/states
+fusermount3 -u /home/${TARGET_USERNAME}/.config/retroarch/saves
