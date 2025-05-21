232f742abb64c7aaddb6617a65375b970acdfb97
Alain Pham <apham@work>
2025-05-22 00:38:56 +0200

    vconv

--- scripts/ffmpeg/vconv-mkvmerge.sh
+++ scripts/ffmpeg/vconv-mkvmerge.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Usage:
-# ./merge_audio.sh <input_video> <input_audio> <language_code> [audio_delay_ms] [set_default]
+# ./merge_audio.sh <input_video> <input_audio> <language_code> [AUDIO_DELAY_S] [set_default]
 # Examples:
 # ./merge_audio.sh input1.mp4 input2.mka en               (delay=0, set_default=yes)
 # ./merge_audio.sh input1.mp4 input2.mka en 300 no       (delay=300ms, no default)
 
 # Check minimum arguments (3 mandatory, 2 optional)
 if [ $# -lt 3 ] || [ $# -gt 5 ]; then
-  echo "Usage: $0 <input_video> <input_audio> <language_code> [audio_delay_ms] [set_default]"
-  echo "  audio_delay_ms: optional, default=0 (milliseconds)"
+  echo "Usage: $0 <input_video> <input_audio> <language_code> [AUDIO_DELAY_S] [set_default]"
+  echo "  AUDIO_DELAY_S: optional, default=0 (milliseconds)"
   echo "  set_default: optional, default='yes' ('yes' or 'no')"
   exit 1
 fi
@@ -17,7 +17,7 @@ fi
 INPUT1="$1"
 INPUT2="$2"
 LANG_CODE="$3"
-AUDIO_DELAY_MS="${4:-0}"      # default to 0 if not given
+AUDIO_DELAY_S="${4:-0}"      # default to 0 if not given
 SET_DEFAULT="${5:-y}"       # default to yes if not given
 
 # Generate output filename (always mkv)
@@ -32,9 +32,9 @@ AUDIO_COUNT=$(ffprobe -v error -select_streams a -show_entries stream=index \
 CMD="ffmpeg -i \"$INPUT1\""
 
 # Add input2 with delay if not zero
-if [ "$AUDIO_DELAY_MS" -ne 0 ]; then
-  AUDIO_DELAY_SEC=$(awk "BEGIN {printf \"%.3f\", $AUDIO_DELAY_MS / 1000}")
-  CMD+=" -itsoffset $AUDIO_DELAY_SEC -i \"$INPUT2\""
+if [ "$AUDIO_DELAY_S" -ne 0 ]; then
+  
+  CMD+=" -itsoffset $AUDIO_DELAY_S -i \"$INPUT2\""
 else
   CMD+=" -i \"$INPUT2\""
 fi
