2df1bf43ee1bba2534b96290dbceb59d30744976
Alain Pham <apham@work>
2025-06-11 16:43:14 +0200

    kvmworkstation test

--- setup-scripts/core.sh
+++ setup-scripts/core.sh
@@ -1095,9 +1095,11 @@ fi
 
 echo "install kube"
 
-cat << EOF | chroot ${ROOTFS}
-    curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik" sh -
-EOF
+curl -Lo ${ROOTFS}/usr/local/bin/k3s https://github.com/k3s-io/k3s/releases/download/${K3S_VERSION}/k3s
+
+# cat << EOF | chroot ${ROOTFS}
+#     curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik" sh -
+# EOF
 
 cat << EOF | chroot ${ROOTFS}
     curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
@@ -2650,6 +2652,28 @@ inumlocktty
 idev
 idocker
 ikube
+sudo reboot now
+}
+
+kvmworkstation(){
+init apham "NA" "authorized_keys" "NA" "NA" "NA"
+bashaliases
+smalllogs
+reposrc
+iaptproxy
+iessentials
+ikeyboard
+inumlocktty
+idev
+idocker
+ikube
+igui
+inumlocktty
+ivmgui
+itheming
+iworkstation
+iautologin
+itimezone
 sudo reboot now
 }
 
