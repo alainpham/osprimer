1a779cc7d01653efcc1c941f6996142425e45890
apham <admin@zez.duckdns.org>
2025-06-16 18:34:31 +0200

    vm scripts

--- setup-scripts/core.sh
+++ setup-scripts/core.sh
@@ -2808,7 +2808,9 @@ idev
 idocker
 ikube
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik" sh -
+cat << EOF | chroot ${ROOTFS}
+    curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik" sh -
+EOF
 
 isecret
 cleanupapt
@@ -2839,7 +2841,11 @@ isudo
 allowsshpwd
 ikeyboard
 ikube
-curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik" sh -
+
+cat << EOF | chroot ${ROOTFS}
+    curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik" sh -
+EOF
+
 isecret
 cleanupapt
 unmountraw
