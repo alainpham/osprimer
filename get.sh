e5b699179a746a5a1805c0056b7f9e6af25f804c
apham <admin@zez.duckdns.org>
2025-08-26 11:31:44 +0200

    get.sh

--- get.sh
+++ get.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf debian-os-image
+rm core.sh
 git clone https://github.com/alainpham/debian-os-image.git
-cd debian-os-image/setup-scripts
+ln -s debian-os-image/setup-scripts/core.sh ./core.sh
