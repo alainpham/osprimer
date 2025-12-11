4d9080ef86eb73ffec6487353407343340ed92ad
apham <admin@houze.dns.army>
2025-12-11 23:44:22 +0100

    kubecr by default local

--- setup-scripts/core.sh
+++ setup-scripts/core.sh
@@ -686,7 +686,7 @@ cat << EOF | chroot ${ROOTFS} ${CHROOT_BASH}
 EOF
 
 cat << EOF | chroot ${ROOTFS} ${CHROOT_BASH}
-    curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik" sh -
+    curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE="true" INSTALL_K3S_SKIP_START="true" INSTALL_K3S_VERSION="${K3S_VERSION}" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=servicelb,traefik --bind-address 172.17.0.1 --node-ip 172.17.0.1 --advertise-address 172.17.0.1" sh -
 EOF
 
 cat << EOF | chroot ${ROOTFS} ${CHROOT_BASH}
