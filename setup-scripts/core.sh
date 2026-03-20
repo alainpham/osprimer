c92c95bee75dec207d03cf5f0ba08f035f641495
alainpham <13981031+alainpham@users.noreply.github.com>
2026-03-20 09:42:06 +0100

    kube version

--- setup-scripts/core.sh
+++ setup-scripts/core.sh
@@ -12,7 +12,7 @@ inputversions() {
     echo "export MAJOR_KUBE_VERSION=${MAJOR_KUBE_VERSION}"
     
     # https://github.com/k3s-io/k3s/releases
-    export K3S_VERSION="v1.34.3+k3s3"
+    export K3S_VERSION="v1.34.4+k3s1"
     echo "export K3S_VERSION=${MAJOR_KUBE_VERSION}"
 
     # https://github.com/derailed/k9s/releases
